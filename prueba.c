ef3ffa821735f44bdbe59383e55060e1d10da722
Roberto-Ulloa <111545962+RobertoUlloa123@users.noreply.github.com>
2023-07-10 15:18:19 -0500

    leer el archivo

--- prueba.c
+++ prueba.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char const *argv[])
 {
-
+    FILE *archivo;
+    char caracter;
+    archivo = fopen("alumnos.txt", "r");
+    while (!feof(archivo))
+    {
+        caracter = fgetc(archivo);
+        putchar(caracter);
+    }
+    fclose(archivo);
     return 0;
 }
